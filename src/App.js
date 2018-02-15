3477bba299c99d5d6d48f657864164f8335717f9
877927262 <877927262@qq.com>
2018-02-15 12:12:39 +0800

    加入了react-redux，并且使用了装饰器

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
+// 1.将 connect 从 react-redux 中引入
+import { connect } from 'react-redux'
 import './App.css';
+import { addGun, removeGun, addGunAsync } from './index.redux'
+//使用装饰器
+@connect(
+  //你要state什么属性放到 props 里
+  state =>({num:state}),
+  //你要什么方法，放到props里，自动 dispatch
+  { addGun, removeGun, addGunAsync }
+)
+
 class App extends Component {
   render() {
-    const store = this.props.store;
-    const num = store.getState();
-    const addGun = this.props.addGun;
-    const removeGun = this.props.removeGun;
-    const addGunAsync = this.props.addGunAsync;
-
     return (
+      // 3.在代码里就能使用 this.props 了
       <div>
-        <h1>现在有机枪{num}把</h1>
-        <button onClick={()=>store.dispatch(addGun())}>申请武器</button>
-        <button onClick={()=>store.dispatch(removeGun())}>上交武器</button>
-        <button onClick={()=>store.dispatch(addGunAsync())}>拖两天再给</button>
+        <h1>现在有机枪{this.props.num}把</h1>
+        <button onClick={this.props.addGun}>申请武器</button>
+        <button onClick={this.props.removeGun}>上交武器</button>
+        <button onClick={this.props.addGunAsync}>拖两天再给</button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,19 +4,20 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 //使用 applyMiddleware 支持异步
-// 1. 引入 applyMiddleware
+// 引入 applyMiddleware
 import { createStore, applyMiddleware } from 'redux';
-import { counter, addGun, removeGun, addGunAsync} from './index.redux'
+import { counter } from './index.redux'
+//1.从 react-redux 中引入 Provider
+import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 // 通过 reducer 创建一个 store , 2.并且使用异步中间件
 const store = createStore(counter,applyMiddleware(thunk))
 
-function render(){
-  // 将 store 传递给 app 组件
-  ReactDOM.render(<App store={store} addGun={addGun} removeGun={removeGun} addGunAsync={addGunAsync}/>, document.getElementById('root'));
-}
+ReactDOM.render(
+  // 2.使用 Provider 包裹 app 组件，并且将 store 传递给 provider
+  <Provider store={store}>
+    <App/>
+  </Provider>
+  , document.getElementById('root'));
 
-render()
-// 当状态改变时，重新渲染组件
-store.subscribe(render);
 registerServiceWorker();
